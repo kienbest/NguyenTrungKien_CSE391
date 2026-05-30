e5861c705b440d0078c663783fbdeebea077b3de
Trung Kien Auto Test <kn0335661179@gmail.com>
2026-05-30 14:34:42 +0700

    hoàn thành tier 0

---
+++ session4/hello-react/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="product">
+      <h1>iphone15</h1>
+      <p className="price">25,000,000 VND</p>
+      <ul>
+        <li>màn hình : 6.1 inch</li>
+        <li>Camera:480Mp</li>
+        <li>Pin: 3349 mAh</li>
+        
+      </ul>
+      <button>Mua ngay</button>
+    </div>
+  );
+}
+
+export default App
+
+// 1, File .jsx khác gì file .js?
+
+// `.js`                                      | `.jsx`                          |
+// | ------------------------------------------ | ------------------------------- |
+// | Chứa JavaScript thuần                      | Chứa JavaScript + JSX           |
+// | Không thường viết trực tiếp HTML bên trong | Có thể viết JSX giống HTML      |
+// | Dùng cho logic JS thông thường             | Thường dùng cho React Component |
+
+// 3. Thử xóa export default App → chuyện gì xảy ra?
+
+// Ví dụ:
+
+// function App() {
+//   return <h1>Hello</h1>
+// }
+
+// Khi chạy:
+
+// npm run dev
+
+// React sẽ báo lỗi vì:
+
+// import App from './App.jsx'
+
+// đang cố lấy một giá trị mặc định (default export), nhưng file App.jsx không cung cấp.
+
+// Lỗi thường gặp:
+
+// The requested module '/src/App.jsx'
+// does not provide an export named 'default'
+
+// Hoặc:
+
+// Failed to resolve import
+
+// Kết quả:
+
+// Trang web không hiển thị bình thường.
+// Vite hiện màn hình lỗi màu đỏ.
+// React không render được component App.
+
+// 2. Tại sao phải export default App?
+
+// Vì component App cần được sử dụng ở file khác.
+
+// Trong project React của bạn, file:
+
+// src/main.jsx
+
+// có đoạn:
+
+// import App from './App.jsx'
+
+// Dòng này yêu cầu file App.jsx phải xuất (export) ra một component tên App.
+
+// Do đó cuối file phải có:
+
+// export default App
+
+// để cho phép file khác import và sử dụng.
