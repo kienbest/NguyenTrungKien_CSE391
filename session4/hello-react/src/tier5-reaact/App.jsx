35cb143bf85c779d837f0c9970c88154ff0b9667
Trung Kien Auto Test <kn0335661179@gmail.com>
2026-05-31 19:54:46 +0700

    complete tier 5

---
+++ session4/hello-react/src/tier5-reaact/App.jsx
@@ -0,0 +1,23 @@
+import ClickEvents from "./ClickEvents";
+import InputEvents from "./InputEvents";
+import KeyboardEvents from "./KeyboardEvents";
+import FormEvents from "./FormEvents";
+
+function App() {
+  return (
+    <>
+      <ClickEvents />
+      <hr />
+
+      <InputEvents />
+      <hr />
+
+      <KeyboardEvents />
+      <hr />
+
+      <FormEvents />
+    </>
+  );
+}
+
+export default App;
